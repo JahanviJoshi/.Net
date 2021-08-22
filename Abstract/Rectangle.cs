665c9ad7bd95985b6c02813c1386447d41dd92b7
JahanviJoshi <83712492+JahanviJoshi@users.noreply.github.com>
2021-08-22 19:55:00 +0530

    Add files via upload

---
+++ Abstract/Rectangle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstract_Class
+{
+    class Rectangle : Shape
+    {
+        public override int area()
+        {
+            return (width * length);
+        }
+    }
+}
---
+++ Abstract/Shape.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstract_Class
+{
+    abstract class Shape
+    {
+        public int length;
+        public int width;
+        public void set(int a = 0, int b = 0)
+        {
+            length = a;
+            width = b;
+        }
+        public abstract int area();
+    }
+
+    class rectangle : Shape
+    {
+        public override int area()
+        {
+            return (width * length);
+        }
+
+    }
+}
